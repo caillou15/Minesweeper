82e7e3d6ef66a9c23af61776658bf442c503bde6
LeRoiDesKiwis <lekiwidu17@gmail.com>
2022-02-01 20:54:20 +0100

    Change readme

--- src/main/java/fr/leroideskiwis/minesweeper/Main.java
+++ src/main/java/fr/leroideskiwis/minesweeper/Main.java
@@ -3,7 +3,7 @@ package fr.leroideskiwis.minesweeper;
 public class Main {
 
     public static void main(String[] args) {
-        GameMap gameMap = new GameMap(9, 9, 5);
+        GameMap gameMap = new GameMap(9, 9, 50);
         System.out.println(gameMap);
     }
 
