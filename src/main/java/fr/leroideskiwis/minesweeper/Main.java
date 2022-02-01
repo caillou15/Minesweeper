5d0b65b0a5e51c8ddb426702d204d62640cad9b4
LeRoiDesKiwis <lekiwidu17@gmail.com>
2022-02-01 18:31:16 +0100

    add Main.java

---
+++ src/main/java/fr/leroideskiwis/minesweeper/Main.java
@@ -0,0 +1,10 @@
+package fr.leroideskiwis.minesweeper;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GameMap gameMap = new GameMap(9, 9, 5);
+        System.out.println(gameMap);
+    }
+
+}
