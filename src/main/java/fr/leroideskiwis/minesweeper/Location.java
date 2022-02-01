a25ecb12d46df1c983cb8413eb404e7e6ebf116f
LeRoiDesKiwis <lekiwidu17@gmail.com>
2022-02-01 17:45:37 +0100

    add reveal() and add equals() to location

--- src/main/java/fr/leroideskiwis/minesweeper/Location.java
+++ src/main/java/fr/leroideskiwis/minesweeper/Location.java
@@ -10,6 +10,25 @@ public class Location {
         this.y = y;
     }
 
+    public Location add(int x, int y){
+        return new Location(this.x+x, this.y+y);
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        Location location = (Location) o;
+
+        if (x != location.x) return false;
+        return y == location.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
 }
